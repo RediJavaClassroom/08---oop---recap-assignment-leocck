0de5db1b994285480cb0946fea2405a4757cf07f
Leonardo Kasperavičius <leocck@gmail.com>
2022-04-13 21:00:44 +0200

    Add partial solution for steps 1 and 2

---
+++ src/main/java/com/redi/j2/Movie.java
@@ -0,0 +1,38 @@
+package com.redi.j2;
+
+public class Movie {
+
+    private String title;
+    private String category;
+    private Rating rating;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public Movie(String title) {
+        this.title = title;
+        category = "Default";
+        rating = new Rating();
+    }
+
+    public void addRating(int value) {
+        rating.addRating(value);
+    }
+
+    public Double getRating() {
+        return rating.getRating();
+    }
+}
